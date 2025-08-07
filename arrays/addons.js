82375f6ab276f233c6b328cbf592005a6a16164f
Orzubek2009 <orzubek200919@gmail.com>
2025-08-06 23:01:35 -0500

    Add files via upload

---
+++ arrays/addons.js
@@ -0,0 +1,13 @@
+const addons = [
+  {
+    "author": "MCBEADDONS",
+    "name": "Test Addon",
+    "icon": "//addonIcons/Minecraft2025-02-2820-48-32-ezgif.com-speed (1) (1) (1) (1)-1754537848653.gif",
+    "description": "Testing and adding new ways to improve MCBEADDONS. Please be patient.",
+    "download": "",
+    "types": [
+      "addon"
+    ],
+    "outdated": false
+  }
+];
